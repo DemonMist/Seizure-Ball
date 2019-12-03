0c5680f6f2c6c2b3f1d112f6f73f5e4ab27bb4c6
courtneyjoel <courtneyjoel@HS-PLTW-28.oskyschools.us>
2019-12-03 13:53:51 -0600

    YEET

--- Ball2/src/Ball2.java
+++ Ball2/src/Ball2.java
@@ -3,9 +3,18 @@ import java.awt.Graphics;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.*;
+import java.awt.Graphics;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.ImageIcon;
+import java.awt.Color;
+
+import javax.swing.JPanel;
+import javax.swing.Timer;
 import java.util.Random;
 
-public  class Ball2 extends JPanel {
+public  class Ball2 extends JPanel implements ActionListener, KeyListener {
 	  Random rand=new Random();
       int x = 0, y = 0;
       int anglex =100, angley = 100;
@@ -13,13 +22,13 @@ public  class Ball2 extends JPanel {
     
           private void move() {
               if (x + anglex < 0) {
-                  anglex = rand.nextInt(100);
+                  anglex = rand.nextInt(10);
              } else if (x + anglex > getWidth() - 50) {
-                  anglex = -rand.nextInt(30);
+                  anglex = -rand.nextInt(600);
              } else if (y + angley < 0) {
-                  angley = rand.nextInt(75);
+                  angley = rand.nextInt(600);
              } else if (y + angley > getHeight() - 50) {
-                  angley = -rand.nextInt(37);
+                  angley = -rand.nextInt(600);
              }
           
       x = x + anglex;
@@ -47,6 +56,30 @@ public  class Ball2 extends JPanel {
          Thread.sleep(10);
          }
 }
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
 
 	}
 
